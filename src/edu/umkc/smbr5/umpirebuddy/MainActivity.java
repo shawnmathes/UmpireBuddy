e638c9bbb9573c3b45b7ae222ccd197ce2c93822
Shawn Mathes <shawnmathes@gmail.com>
2014-02-15 00:48:50 -0600

    Add total outs and persist data (Part 3).

--- src/edu/umkc/smbr5/umpirebuddy/MainActivity.java
+++ src/edu/umkc/smbr5/umpirebuddy/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +13,12 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
+    private static final String PREFS_NAME = "PrefsFile";
+    private static final String TOTAL_OUTS_INDEX = "total_outs_index";
+    
     private int strike_count;
     private int ball_count;
+    private int total_outs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,16 @@ public class MainActivity extends Activity implements OnClickListener {
 
         View ballButton = findViewById(R.id.ball_button);
         ballButton.setOnClickListener(this);
+        
+        if (savedInstanceState != null) {
+            total_outs = savedInstanceState.getInt(TOTAL_OUTS_INDEX, 0);
+        } else {
+            // Restore preferences
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            // The first time preferences will not exist and max
+            // will be assigned 0 (the second argument)
+            total_outs = settings.getInt(TOTAL_OUTS_INDEX, 0);
+        }
 
         reset();
     }
@@ -50,6 +65,24 @@ public class MainActivity extends Activity implements OnClickListener {
                 return super.onOptionsItemSelected(item);
         }
     }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TOTAL_OUTS_INDEX, total_outs);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(TOTAL_OUTS_INDEX, total_outs);
+
+        // Commit the edits
+        editor.commit();
+    }
 
     private void reset() {
         strike_count = 0;
@@ -68,6 +101,7 @@ public class MainActivity extends Activity implements OnClickListener {
             if (strike_count < 2) {
                 strike_count++;
             } else {
+                total_outs++;
                 alertAndReset(R.string.strike_out_text);
             }
             break;
@@ -101,5 +135,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
         TextView b = (TextView) findViewById(R.id.ball_textview);
         b.setText(Integer.toString(ball_count));
+        
+        TextView t = (TextView) findViewById(R.id.total_outs_textview);
+        t.setText(Integer.toString(total_outs));
     }
 }
